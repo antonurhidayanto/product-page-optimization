a592adfc2f3ff4d6df5bfbdd261c9ae95ceb7b9a
antonurhidayanto <nurhidayanto.nurhidayanto@gmail.com>
2025-10-01 17:21:27 +0700

    create product card

--- product-page-optimization/src/app/components/ProductCard.tsx
+++ product-page-optimization/src/app/components/ProductCard.tsx
@@ -1,5 +1,31 @@
+import Link from "next/link";
 
+type Product = {
+  id: string | number;
+  images: string;
+  title: string;
+  description: string;
+  price: number;
+};
 
-const ProductCard = () => {
-  return 
-}
+type ProductCardProps = {
+  product: Product;
+};
+
+const ProductCard = ({ product }: any) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-xs mb-3">
+      <img src={product.images} alt={product.title} />
+      <div className="p-4">
+        <h2 className="text-lg font-semibold truncate">{product.title}</h2>
+        <p className="text-sm text-gray-600 mt-2">{product.description}</p>
+        <p className="text-xl font-bold text-gray-900 mt-4">Price: ${product.price}</p>
+      </div>
+      <div className="p-4">
+        <Link className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded" href={`/products/${product.id}`}>View Details</Link>  
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
